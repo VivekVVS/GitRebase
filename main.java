708a8a5e23f6b8185dad4c12204fb1b74b511dab
vivek <viveksalunke.me@gmail.com>
2022-07-13 18:34:24 +0530

    js;la

--- main.java
+++ main.java
@@ -2,7 +2,12 @@ public void maine() {
     system.out.println("This is the maine method");
     
 }
+<<<<<<< HEAD
 public void brancha() {
     system.out.println("This is the brancha method");
+=======
+public void brachb() {
+    system.out.println("This is the branchb method");
+>>>>>>> 141aeb5 (brachb mehtod added)
     
 }
